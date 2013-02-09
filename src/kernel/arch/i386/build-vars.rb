ed400522a2fea94e04ae59e7ec9e43ec00a7855c
Max Reitz <xanclic@googlemail.com>
2013-02-09 22:29:28 +0100

    Backtrace
    + Backtrace für kassert

--- src/kernel/arch/i386/build-vars.rb
+++ src/kernel/arch/i386/build-vars.rb
@@ -1,7 +1,7 @@
 {
     cc: {
         cmd: 'gcc',
-        flags: "-m32 -masm=intel -O3 -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-ssse3 -mno-sse4 -mno-sse4.1 -mno-sse4.2 -mno-sse4a -mno-avx -mno-avx2 -mno-3dnow -ffreestanding -nostartfiles -nostdinc -nodefaultlibs -g2 #{$inc_dirs.map { |d| "'-I#{d}'" } * ' '} -Wall -Wextra -pedantic -std=gnu1x -funsigned-char -fno-stack-protector -DKERNEL -DX86 -DI386 -DLITTLE_ENDIAN -DHAS_VMEM -Wdouble-promotion -Wformat=2 -Winit-self -Wmissing-include-dirs -Wswitch-enum -Wsync-nand -Wunused -Wtrampolines -Wundef -Wno-endif-labels -Wshadow -Wwrite-strings -Wlogical-op -Waggregate-return -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wmissing-declarations -Wnormalized=nfc -Wnested-externs -Winvalid-pch -Wdisabled-optimization -Woverlength-strings" # -fno-omit-frame-pointer"
+        flags: "-m32 -masm=intel -O3 -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-ssse3 -mno-sse4 -mno-sse4.1 -mno-sse4.2 -mno-sse4a -mno-avx -mno-avx2 -mno-3dnow -ffreestanding -nostartfiles -nostdinc -nodefaultlibs -g2 #{$inc_dirs.map { |d| "'-I#{d}'" } * ' '} -Wall -Wextra -pedantic -std=gnu1x -funsigned-char -fno-stack-protector -DKERNEL -DX86 -DI386 -DLITTLE_ENDIAN -DHAS_VMEM -Wdouble-promotion -Wformat=2 -Winit-self -Wmissing-include-dirs -Wswitch-enum -Wsync-nand -Wunused -Wtrampolines -Wundef -Wno-endif-labels -Wshadow -Wwrite-strings -Wlogical-op -Waggregate-return -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wmissing-declarations -Wnormalized=nfc -Wnested-externs -Winvalid-pch -Wdisabled-optimization -Woverlength-strings -fno-omit-frame-pointer"
     },
     asm: {
         cmd: 'fasm',
