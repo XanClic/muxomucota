92eeccb584594507a2d8de047d35151d4013eb93
Hanna Reitz <xanclic@xanclic.moe>
2013-02-10 00:01:52 +0100

    Sanity Checks für vmmc_make_shm
    ! Zwei kleine Tests, ob die übergebenen virtuellen Adressen überhaupt
      hinhauen können.

--- src/include/common/pmm.h
+++ src/include/common/pmm.h
@@ -13,6 +13,7 @@ void pmm_free(uintptr_t paddr);
 void pmm_use(uintptr_t paddr);
 
 int pmm_user_count(uintptr_t paddr);
+bool pmm_alloced(uintptr_t paddr);
 
 void pmm_mark_cow(uintptr_t paddr, bool flag);
 bool pmm_is_cow(uintptr_t paddr);
