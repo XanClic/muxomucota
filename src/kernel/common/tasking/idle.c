5fa98ecb07ffec77ac10208a73b79bc9304f31a4
Max Reitz <xanclic@googlemail.com>
2013-02-11 18:40:38 +0100

    Kooperatives MT: Leerlaufprozess
    * Untersucht jetzt die Runqueue, ob diese leer ist. Wenn ja, wird
      cpu_halt() ausgeführt, sonst yield().

--- src/kernel/common/tasking/idle.c
+++ src/kernel/common/tasking/idle.c
@@ -1,12 +1,14 @@
 #include <cpu.h>
+#include <ipc.h>
 #include <lock.h>
 #include <process.h>
+#include <stdbool.h>
 #include <stddef.h>
 
 
-extern process_t *dead;
+extern process_t *dead, *runqueue;
 
-extern lock_t dead_lock;
+extern lock_t runqueue_lock;
 
 
 void enter_idle_process(void)
@@ -18,7 +20,31 @@ void enter_idle_process(void)
         if (dead == NULL)
         {
 #ifdef COOPERATIVE
-            yield();
+            bool runqueue_empty = true;
+
+            lock(&runqueue_lock);
+
+            if (runqueue != NULL)
+            {
+                process_t *proc = runqueue;
+                do
+                {
+                    if (proc->status == PROCESS_ACTIVE)
+                    {
+                        runqueue_empty = false;
+                        break;
+                    }
+                    proc = proc->next;
+                }
+                while (proc != runqueue);
+            }
+
+            unlock(&runqueue_lock);
+
+            if (runqueue_empty)
+                cpu_halt();
+            else
+                yield();
 #else
             cpu_halt();
 #endif
