d64b65437271741450b65b11a52acdfc169c0f40
Hanna Reitz <xanclic@xanclic.moe>
2013-02-18 21:13:23 +0100

    Kernel-Lock: Immer yielden
    ! Da die IRQ-Behandlung nun (hoffentlich) lockfrei stattfindet, kann der
      Fall (hoffentlich) nicht mehr eintreten, dass ein Prozess im Kernel
      versucht, einen von sich selbst gehaltenen Lock nochmal zu locken

--- src/kernel/arch/i386/lib/lock.c
+++ src/kernel/arch/i386/lib/lock.c
@@ -9,12 +9,7 @@
 void lock(volatile lock_t *v)
 {
     while (__sync_lock_test_and_set(&v->status, 1))
-    {
-        // Klingt merkwürdig, kann in IRQ-Handlern aber passieren, ohne dass es
-        // schlimm wäre. Wenn wir den Fall so abfangen, zumindest.
-        if ((current_process == NULL) || (current_process->pid != v->owner))
-            yield_to(v->owner);
-    }
+        yield_to(v->owner);
 
     v->owner = (current_process == NULL) ? -1 : current_process->pid;
 }
