4eaa79ee21abf188d4a164e77ef99a81d4cce60d
Max Reitz <xanclic@gmail.com>
2013-02-27 22:51:39 +0100

    RW-Locks
    + Locks, die zwischen lesendem und schreibendem Zugriff unterscheiden

--- src/include/common/lock.h
+++ src/include/common/lock.h
@@ -36,4 +36,27 @@ void lock(volatile lock_t *v);
 void unlock(volatile lock_t *v);
 bool trylock(volatile lock_t *v);
 
+
+#ifndef KERNEL
+
+// readers-writers
+typedef struct
+{
+    lock_t write_lock, lock_lock;
+    int reader_count;
+} rw_lock_t;
+
+#define RW_LOCK_INITIALIZER { .write_lock = LOCK_INITIALIZER, .reader_count = 0 }
+
+static inline void rwl_lock_init(rw_lock_t *rwl) { lock_init(&rwl->write_lock); lock_init(&rwl->lock_lock); rwl->reader_count = 0; }
+
+void rwl_lock_r(volatile rw_lock_t *rwl);
+void rwl_unlock_r(volatile rw_lock_t *rwl);
+void rwl_lock_w(volatile rw_lock_t *rwl);
+void rwl_unlock_w(volatile rw_lock_t *rwl);
+void rwl_require_w(volatile rw_lock_t *rwl);
+void rwl_drop_w(volatile rw_lock_t *rwl);
+
+#endif
+
 #endif
