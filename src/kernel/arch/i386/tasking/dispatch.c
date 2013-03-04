29fc1226aadc0bd0018804c1c8cf4c5b68218f2e
Max Reitz <xanclic@gmail.com>
2013-03-04 19:33:31 +0100

    fxsave/fxrstor
    + #NM auch auswerten

--- src/kernel/arch/i386/tasking/dispatch.c
+++ src/kernel/arch/i386/tasking/dispatch.c
@@ -1,11 +1,15 @@
 #include <cpu-state.h>
 #include <ipc.h>
+#include <lock.h>
 #include <process.h>
 #include <tls.h>
 #include <x86-segments.h>
 
 
 extern struct tss x86_tss;
+
+static process_t *last_fpu_sse_user = NULL;
+static lock_t last_fpu_sse_user_lock = LOCK_INITIALIZER;
 
 
 void arch_process_change(process_t *new_process)
@@ -15,4 +19,34 @@ void arch_process_change(process_t *new_process)
     set_tls(new_process->tls);
 
     __asm__ __volatile__ ("mov eax,cr0; or eax,8; mov cr0,eax" ::: "eax"); // TS setzen
+}
+
+
+void save_and_restore_fpu_and_sse(void)
+{
+    __asm__ __volatile__ ("mov eax,cr0; and eax,~8; mov cr0,eax" ::: "eax"); // TS löschen
+
+
+    lock(&last_fpu_sse_user_lock);
+
+    if (last_fpu_sse_user != NULL)
+        __asm__ __volatile__ ("fxsave %0" :: "m"(*last_fpu_sse_user->arch.fxsave));
+
+    last_fpu_sse_user = current_process;
+
+    unlock(&last_fpu_sse_user_lock);
+
+
+    __asm__ __volatile__ ("fxrstor %0" :: "m"(*current_process->arch.fxsave));
+}
+
+
+void fpu_sse_unregister(struct process *proc)
+{
+    lock(&last_fpu_sse_user_lock);
+
+    if (last_fpu_sse_user == proc)
+        last_fpu_sse_user = NULL;
+
+    unlock(&last_fpu_sse_user_lock);
 }
