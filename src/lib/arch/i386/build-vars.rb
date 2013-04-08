b2fc7aea9f5600cdf3bbc67ba93943a3d08b2ce1
Hanna Reitz <xanclic@xanclic.moe>
2013-04-09 01:50:59 +0200

    memcpy
    ! Kernel und Bibliothek mit -O2 kompilieren, da GCC 4.8.0 ansonsten
      memcpy als einen Aufruf zu memcpy umsetzt.

--- src/lib/arch/i386/build-vars.rb
+++ src/lib/arch/i386/build-vars.rb
@@ -1,7 +1,7 @@
 {
     cc: {
         cmd: 'gcc',
-        flags: "-m32 -O3 -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-ssse3 -ffreestanding -nostartfiles -nostdinc -nodefaultlibs -g2 #{$inc_dirs.map { |d| "'-I#{d}'" } * ' '} -Wall -Wextra -pedantic -std=gnu1x -funsigned-char -fno-stack-protector -DX86 -DI386 -DLITTLE_ENDIAN -DHAS_VMEM -Wdouble-promotion -Wformat=2 -Winit-self -Wmissing-include-dirs -Wswitch-enum -Wsync-nand -Wunused -Wtrampolines -Wundef -Wno-endif-labels -Wshadow -Wunsafe-loop-optimizations -Wcast-align -Wwrite-strings -Wlogical-op -Waggregate-return -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wmissing-declarations -Wnormalized=nfc -Wnested-externs -Winvalid-pch -Wdisabled-optimization -Woverlength-strings -fno-omit-frame-pointer"
+        flags: "-m32 -O2 -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-ssse3 -ffreestanding -nostartfiles -nostdinc -nodefaultlibs -g2 #{$inc_dirs.map { |d| "'-I#{d}'" } * ' '} -Wall -Wextra -pedantic -std=gnu1x -funsigned-char -fno-stack-protector -DX86 -DI386 -DLITTLE_ENDIAN -DHAS_VMEM -Wdouble-promotion -Wformat=2 -Winit-self -Wmissing-include-dirs -Wswitch-enum -Wsync-nand -Wunused -Wtrampolines -Wundef -Wno-endif-labels -Wshadow -Wunsafe-loop-optimizations -Wcast-align -Wwrite-strings -Wlogical-op -Waggregate-return -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wmissing-declarations -Wnormalized=nfc -Wnested-externs -Winvalid-pch -Wdisabled-optimization -Woverlength-strings -fno-omit-frame-pointer"
     },
     asm: {
         cmd: 'fasm',
