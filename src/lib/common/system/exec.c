ef4b7e4b1454d284429f078e7e98c331270513f8
Hanna Reitz <xanclic@xanclic.moe>
2013-04-14 23:59:50 +0200

    exec: Shebang erkennen und entsprechend handeln
    + exec(vp) erkennt ab jetzt ein Shebang und führt die Datei dann unter
      Zuhilfenahme des angegebenen Interpreters aus.

--- src/lib/common/system/exec.c
+++ src/lib/common/system/exec.c
@@ -12,6 +12,7 @@ extern char **environ;
 int execvp(const char *file, char *const argv[])
 {
     int fd = -1;
+    char *npath = NULL, *fullpath = (char *)file;
 
     if (strchr(file, '/') != NULL)
         fd = create_pipe(file, O_RDONLY);
@@ -21,7 +22,7 @@ int execvp(const char *file, char *const argv[])
         if (cpvar == NULL)
             cpvar = ".:/bin:/usr/bin";
 
-        char *npath = malloc(strlen(file) + strlen(cpvar) + 2);
+        npath = malloc(strlen(file) + strlen(cpvar) + 2);
         char *pvar = strdup(cpvar);
 
         STRTOK_FOREACH(pvar, ":", pp)
@@ -37,12 +38,16 @@ int execvp(const char *file, char *const argv[])
                 break;
         }
 
-        free(npath);
         free(pvar);
+
+        fullpath = npath;
     }
 
     if (fd < 0)
+    {
+        free(npath);
         return -1;
+    }
 
 
     size_t len = pipe_get_flag(fd, F_PRESSURE);
@@ -52,9 +57,46 @@ int execvp(const char *file, char *const argv[])
     destroy_pipe(fd, 0);
 
 
-    int ret = (int)syscall4(SYS_EXEC, (uintptr_t)mem, len, (uintptr_t)argv, (uintptr_t)environ);
+    int argc;
+    for (argc = 0; argv[argc] != NULL; argc++);
 
+    char *n_argv[argc + 2];
+    n_argv[0] = NULL;
+    memcpy(n_argv + 1, argv, (argc + 1) * sizeof(char *));
+
+    uintptr_t argvp = (uintptr_t)&n_argv[1];
+
+
+    if ((len > 2) && (((char *)mem)[0] == '#') && (((char *)mem)[1] == '!'))
+    {
+        char *interpreter = strtok((char *)mem + 2, "\r\n");
+
+        fd = create_pipe(interpreter, O_RDONLY);
+        if (fd < 0)
+        {
+            free(mem);
+            return -1;
+        }
+
+        n_argv[0] = strdup(interpreter);
+        n_argv[1] = fullpath;
+        argvp = (uintptr_t)n_argv;
+
+        free(mem);
+
+        len = pipe_get_flag(fd, F_PRESSURE);
+        mem = malloc(len);
+        stream_recv(fd, mem, len, 0);
+
+        destroy_pipe(fd, 0);
+    }
+
+
+    int ret = (int)syscall4(SYS_EXEC, (uintptr_t)mem, len, argvp, (uintptr_t)environ);
+
+    free(n_argv[0]);
     free(mem);
+    free(npath);
 
     return ret;
 }
