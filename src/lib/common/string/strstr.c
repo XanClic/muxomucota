8e5f67a6d5b2d5912c51f5c3d9c735424a7866cc
Hanna Reitz <xanclic@xanclic.moe>
2013-04-14 23:58:22 +0200

    strstr: dafuq war das
    ! Meine Güte, das war alles aber kein strstr

--- src/lib/common/string/strstr.c
+++ src/lib/common/string/strstr.c
@@ -6,10 +6,9 @@ char *strstr(const char *s1, const char *s2)
 
     while (*s1)
     {
-        for (i = 0; s2[i] && (s1[i] == s2[i]); i++);
-
-        if (!s2[i])
-            return (char *)s1;
+        for (i = 0; s2[i]; i++)
+            if (s2[i] == *s1)
+                return (char *)s1;
 
         s1++;
     }
