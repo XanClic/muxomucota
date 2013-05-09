0192a2992e1bd0cd7ee0474dbe2b6753b3a5982e
Hanna Reitz <xanclic@xanclic.moe>
2013-05-09 02:01:33 +0200

    mount: Automatischer Doppel-mount
    + Wird ein Dateisystemtyp angegeben, bindet mount die Datei erst unter
      dem Service mit diesem Namen ein und den entstehenden Mountpoint dann
      unter dem zweiten angegebenen Ort (im Allgemeinen unter (root)).
      Dadurch wird die unixübliche -t-Schreibweise ermöglicht:
      $ mount -t tar (mboot)/boot/initrd.tar /
      entspricht:
      $ mount (mboot)/boot/initrd.tar (tar)/_mount…
      $ mount (tar)/_mount… /
      Der -t-Parameter ist jetzt allerdings nicht mehr optional, da ein
      mount nach (root) praktisch immer erfolgreich ist, aber etwas völlig
      anderes als automatische Dateisystemerkennung ist (-t vfs führt zum
      alten Verhalten).

--- src/progs/common/mount/mount.c
+++ src/progs/common/mount/mount.c
@@ -17,13 +17,17 @@
  * along with µxoµcota.  If not, see <http://www.gnu.org/licenses/>.    *
  ************************************************************************/
 
+#include <errno.h>
 #include <shm.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <system-timer.h>
+#include <unistd.h>
 #include <vmem.h>
 #include <vfs.h>
+#include <sys/wait.h>
 
 
 extern char *_cwd; // FIXME
@@ -41,23 +45,105 @@ static bool is_absolute_path(const char *path)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    const char *file = NULL, *mountpoint = NULL, *fs_type = NULL;
+
+    for (int i = 1; i < argc; i++)
     {
-        fprintf(stderr, "Usage: mount <file> <mountpoint>\n");
+        if (argv[i][0] == '-')
+        {
+            if (strcmp(argv[i], "-t"))
+            {
+                fprintf(stderr, "Unknown parameter “%s”.\n", argv[i]);
+                return 1;
+            }
+
+            if (i == argc - 1)
+            {
+                fprintf(stderr, "Parameter expected for “-t”.\n");
+                return 1;
+            }
+
+            fs_type = argv[++i];
+        }
+        else if (!file)
+            file = argv[i];
+        else if (!mountpoint)
+            mountpoint = argv[i];
+        else
+        {
+            fprintf(stderr, "Unexpected parameter “%s”.\n", argv[i]);
+            return 1;
+        }
+    }
+
+    if (!file || !mountpoint)
+    {
+        fprintf(stderr, "Usage: mount <file> -t fs-type <mountpoint>\n");
         return 1;
     }
 
-    int fd = create_pipe(argv[2], O_CREAT_MOUNT_POINT);
+    if (!fs_type)
+    {
+        fprintf(stderr, "There is no FS type autodetection; the FS type has to be specified via “-t”.\n");
+        fprintf(stderr, "In order to do a µxoµcota-specific mount, use “-t vfs”.\n");
+        return 1;
+    }
+
+    // TODO: Überprüfen, dass nur eine der beiden folgenden Möglichkeiten zutrifft:
+    //  - fs-type ≠ "vfs" und mountpoint ist auf (root) (oder /)
+    //  - fs-type = "vfs" und mountpoint-Service ist explizit gegeben und nicht (root)
+
+
+    if (strcmp(fs_type, "vfs"))
+    {
+        char *temp_mp;
+
+        for (;;)
+        {
+            asprintf(&temp_mp, "(%s)/_mount%i", fs_type, elapsed_ms());
+
+            int fd = create_pipe(temp_mp, O_JUST_STAT);
+
+            if (fd >= 0)
+                destroy_pipe(fd, 0);
+            else
+                break;
+        }
+
+
+        pid_t child;
+
+        if (!(child = fork()))
+        {
+            printf("mount '%s' '%s'\n", file, temp_mp);
+            execlp("mount", "mount", file, temp_mp, NULL);
+        }
+
+        waitpid(child, NULL, 0);
+
+        if (!(child = fork()))
+        {
+            printf("mount '%s' '%s'\n", temp_mp, mountpoint);
+            execlp("mount", "mount", temp_mp, mountpoint, NULL);
+        }
+
+        waitpid(child, NULL, 0);
+
+
+        return 0;
+    }
+
+
+    int fd = create_pipe(mountpoint, O_CREAT_MOUNT_POINT);
 
     if (fd < 0)
     {
-        perror("Could not open file");
+        perror("Could not create mount point");
         return 1;
     }
 
     if (!pipe_implements(fd, I_FS))
     {
-        destroy_pipe(fd, 0);
         fprintf(stderr, "Target directory does not implement the filesystem interface.\n");
         return 1;
     }
@@ -65,22 +151,19 @@ int main(int argc, char *argv[])
 
     big_size_t ret;
 
-    if (is_absolute_path(argv[1]))
-        ret = stream_send(fd, argv[1], strlen(argv[1]) + 1, F_MOUNT_FILE);
+    if (is_absolute_path(file))
+        ret = stream_send(fd, file, strlen(file) + 1, F_MOUNT_FILE);
     else
     {
-        size_t fplen = strlen(_cwd) + 1 + strlen(argv[1]) + 1;
+        size_t fplen = strlen(_cwd) + 1 + strlen(file) + 1;
 
         char full_path[fplen];
         strcpy(full_path, _cwd);
         strcat(full_path, "/");
-        strcat(full_path, argv[1]);
+        strcat(full_path, file);
 
         ret = stream_send(fd, full_path, fplen, F_MOUNT_FILE);
     }
-
-    destroy_pipe(fd, 0);
-
 
     if (!ret)
         fprintf(stderr, "Could not mount.\n");
