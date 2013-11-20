4f403eb426a5c64755c14eb74ab71e61792a9690
Max Reitz <xanclic@gmail.com>
2013-11-20 22:40:35 +0100

    kmalloc: Ordentliche Implementation
    + Implementation mit verschiedenen Speicherpools (sollte letztendlich
      auch in den Userspace verlagert werden) und auf Wunsch (#ifdef) mit
      Guards.

--- src/include/common/digest.h
+++ src/include/common/digest.h
@@ -32,10 +32,11 @@ digest_t _calculate_checksum(const void *buffer, size_t size);
 bool _check_integrity(const void *buffer, size_t size);
 
 
-// Beide Makros erfordern die Existenz eines „digest_t digest“-Felds in der
+// Alle Makros erfordern die Existenz eines „digest_t digest“-Felds in der
 // Struktur; die Integrität wird nur bis zu diesem Feld gewährleistet.
 #define calculate_checksum(structure) _calculate_checksum(&(structure), (uintptr_t)&((structure).digest) - (uintptr_t)&(structure))
 #define check_integrity(structure) _check_integrity(&(structure), (uintptr_t)&((structure).digest) + sizeof(digest_t) - (uintptr_t)&(structure))
+#define write_checksum(structure) (structure).digest = calculate_checksum(structure)
 
 
 // Fügt besagtes „digest_t digest“-Feld in eine Struktur ein
--- src/kernel/common/memory/kmalloc.c
+++ src/kernel/common/memory/kmalloc.c
@@ -17,32 +17,293 @@
  * along with µxoµcota.  If not, see <http://www.gnu.org/licenses/>.    *
  ************************************************************************/
 
+#include <compiler.h>
+#include <digest.h>
 #include <kassert.h>
 #include <kmalloc.h>
+#include <lock.h>
 #include <pmm.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <vmem.h>
+
+
+// FIXME
+static lock_t alloc_lock = LOCK_INITIALIZER;
+
+
+#define CHECK_INTEGRITY
+
+#ifdef CHECK_INTEGRITY
+#define check(x) kassert_print(check_integrity(*(x)), "%p corrupted", x)
+#define update(x) write_checksum(*(x))
+
+#define from_next(x) ((typeof(*x))((uintptr_t)(x) - offsetof(typeof(**x), next)))
+#else
+#define check(x)
+#define update(x)
+#endif
+
+
+struct pool;
+
+struct block
+{
+    struct block *next;
+    struct pool *pool;
+    size_t size; // size of comple block (including metadata)
+#ifdef CHECK_INTEGRITY
+    DIGESTIFY
+#endif
+};
+
+#define BLOCK_META_SIZE ((sizeof(struct block) + 0xf) & ~0xf)
+
+struct pool
+{
+    struct pool *next;
+    struct block *free_blocks;
+    size_t max_free_size;
+    size_t size;
+#ifdef CHECK_INTEGRITY
+    DIGESTIFY
+#endif
+};
+
+#define POOL_META_SIZE ((sizeof(struct pool) + 0xf) & ~0xf)
+
+static struct pool *pools;
+
+
+static void join_memory(struct pool *pool)
+{
+    check(pool);
+
+    struct block *block = pool->free_blocks;
+
+    if (unlikely(!block))
+        return;
+
+    while (block->next)
+    {
+        check(block);
+        kassert_print(block->pool == pool, "Block %p does not belong to pool %p (but to %p)", block, pool, block->pool);
+
+        kassert_print((uintptr_t)block + block->size > (uintptr_t)block->next, "Overlapping heap blocks (%p + %p > %p; in block %p)", block, block->size, block->next, block->pool);
+
+        if (unlikely((uintptr_t)block + block->size == (uintptr_t)block->next))
+        {
+            check(block->next);
+            kassert_print(block->next->pool == pool, "Block %p does not belong to pool %p (but to %p)", block->next, pool, block->next->pool);
+
+            block->size += block->next->size;
+            block->next  = block->next->next;
+
+            update(block);
+
+            if (block->size > pool->max_free_size)
+            {
+                pool->max_free_size = block->size;
+                update(pool);
+            }
+        }
+        else
+            block = block->next;
+    }
+}
+
+
+static void alloc_pool(size_t sz)
+{
+    kassert(sz >= POOL_META_SIZE + BLOCK_META_SIZE);
+
+    sz = (sz + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
+
+    int frame_count = sz >> PAGE_SHIFT;
+    uintptr_t pageframes[frame_count];
+
+    // Avoid external fragmentation
+    for (int i = 0; i < frame_count; i++)
+        pageframes[i] = pmm_alloc();
+
+    struct pool *pool = kernel_map_nc(pageframes, frame_count);
+    pool->size = sz;
+    pool->max_free_size = sz - POOL_META_SIZE;
+    pool->free_blocks = (struct block *)((uintptr_t)pool + POOL_META_SIZE);
+    pool->free_blocks->next = NULL;
+    pool->free_blocks->pool = pool;
+    pool->free_blocks->size = sz - POOL_META_SIZE;
+
+    struct pool **fp;
+    for (fp = &pools; *fp && (*fp < pool); fp = &(*fp)->next)
+        check(*fp);
+
+    pool->next = *fp;
+    *fp = pool;
+    if (fp != &pools)
+        update(from_next(fp));
+
+    update(pool);
+    update(pool->free_blocks);
+
+
+    bool current_pool_fragmented = false;
+
+    pool = pools;
+    while (pool->next)
+    {
+        check(pool);
+
+        kassert_print((uintptr_t)pool + pool->size <= (uintptr_t)pool->next, "Overlapping heap pools (%p + %p > %p)", pool, pool->size, pool->next);
+
+        if (unlikely((uintptr_t)pool + pool->size == (uintptr_t)pool->next))
+        {
+            struct pool *next = pool->next;
+            check(next);
+            pool->size += next->size;
+            pool->next  = next->next;
+            update(pool);
+
+            struct block *hdr = (struct block *)next;
+            hdr->next = next->free_blocks;
+            hdr->size = POOL_META_SIZE;
+            update(hdr);
+
+            for (struct block *b = hdr; b; b = b->next)
+            {
+                check(b);
+                b->pool = pool;
+                update(b);
+            }
+
+            struct block **bp;
+            for (bp = &pool->free_blocks; *bp; bp = &(*bp)->next)
+                check(*bp);
+
+            *bp = hdr;
+            if (bp != &pool->free_blocks)
+                update(from_next(bp));
+            else
+                update(pool);
+
+            current_pool_fragmented = true;
+        }
+        else
+        {
+            if (unlikely(current_pool_fragmented))
+            {
+                join_memory(pool);
+                current_pool_fragmented = false;
+            }
+
+            pool = pool->next;
+        }
+    }
+}
+
+
+static void update_max_free_size(struct pool *pool)
+{
+    size_t max = 0;
+
+    check(pool);
+
+    for (struct block *block = pool->free_blocks; block; block = block->next)
+    {
+        check(block);
+        if (block->size > max)
+            max = block->size;
+    }
+
+    pool->max_free_size = max;
+    update(pool);
+}
+
+
+static struct block *alloc_block(struct pool *pool, size_t sz)
+{
+    struct block **fb = &pool->free_blocks;
+
+    // There is always a block big enough (verified through max_free_size)
+
+    while (*fb)
+    {
+        check(*fb);
+
+        if ((*fb)->size >= sz)
+        {
+            /*          _         _
+             *         |  HDR      |
+             *         |  [block] _| - sz
+             * osize - |  HDR
+             *         |_ [fill]
+             */
+
+            struct block *t = *fb;
+
+            bool up_mfs = t->size == pool->max_free_size;
+
+            if (t->size - sz < BLOCK_META_SIZE + 16)
+                *fb = t->next;
+            else
+            {
+                size_t osize = t->size;
+                *fb = (struct block *)((uintptr_t)t + sz);
+                (*fb)->size = osize - sz;
+                (*fb)->pool = pool;
+                (*fb)->next = t->next;
+
+                update(*fb);
+
+                t->size = sz;
+                update(t);
+            }
+
+            if (fb != &pool->free_blocks)
+                update(from_next(fb));
+            else
+                update(pool);
+
+            if (up_mfs)
+                update_max_free_size(pool);
+
+            return t;
+        }
+
+        fb = &(*fb)->next;
+    }
+
+    kassert_print(0, "No fitting block found in heap pool %p (%p searched, %p promised)", pool, sz, pool->max_free_size);
+}
 
 
 void *kmalloc(size_t size)
 {
-    size += sizeof(size);
+    if (!size)
+        size = 1;
 
+    size = ((size + 0xf) & ~0xf) + BLOCK_META_SIZE;
 
-    int frame_count = (size + PAGE_SIZE) >> PAGE_SHIFT;
-    uintptr_t pageframes[frame_count];
+    struct pool *pool;
 
-    // Externe Fragmentation vermeiden
-    for (int i = 0; i < frame_count; i++)
-        pageframes[i] = pmm_alloc();
+    lock(&alloc_lock);
 
+    do
+    {
+        for (pool = pools; pool && (pool->max_free_size < size); pool = pool->next)
+            check(pool);
 
-    size_t *mem = kernel_map_nc(pageframes, sizeof(pageframes) / sizeof(pageframes[0]));
+        if (!pool)
+            alloc_pool(size + POOL_META_SIZE);
+    } while (!pool);
 
-    *mem = size;
+    struct block *block = alloc_block(pool, size);
+    check(block);
 
+    unlock(&alloc_lock);
 
-    return mem + 1;
+    return (void *)((uintptr_t)block + BLOCK_META_SIZE);
 }
 
 
@@ -52,15 +313,28 @@ void kfree(void *ptr)
         return;
 
 
-    ptr = (size_t *)ptr - 1;
+    struct block *block = (struct block *)((uintptr_t)ptr - BLOCK_META_SIZE);
+    check(block);
 
-    kassert(!((uintptr_t)ptr & 0xFFF));
+    lock(&alloc_lock);
 
-    size_t size = *(size_t *)ptr;
+    struct pool *pool = block->pool;
+    check(pool);
 
+    struct block **fb;
+    for (fb = &pool->free_blocks; *fb && (*fb < block); fb = &(*fb)->next)
+        check(*fb);
 
-    int frame_count = (size + PAGE_SIZE) >> PAGE_SHIFT;
+    kassert_print(*fb != block, "Double free (%p)", block);
 
-    for (int i = 0; i < frame_count; i++)
-        pmm_free(kernel_unmap((void *)((uintptr_t)ptr + ((uintptr_t)i << PAGE_SHIFT)), PAGE_SIZE));
+    block->next = *fb;
+    *fb = block;
+    if (fb != &pool->free_blocks)
+        update(from_next(fb));
+    else
+        update(pool);
+
+    join_memory(pool);
+
+    unlock(&alloc_lock);
 }
